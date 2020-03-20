ff2be886b20d0d76678700d435f695530c237955
yunsheng <helloyunsheng@126.com>
2020-03-20 17:34:57 +0800

    文件拆分与合并

---
+++ src/test/java/com/example/demo/file/SplitAndMergeTest.java
@@ -0,0 +1,26 @@
+package com.example.demo.file;
+
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.junit.Assert.*;
+
+/**
+ * @作者 技术笔记与开源分享
+ * @时间 2020/3/20
+ * @描述
+ */
+public class SplitAndMergeTest {
+    @Test
+    public void split() throws Exception {
+        File src = new File("src/test/resources/file/src.pdf");
+        SplitAndMerge.split(src);
+    }
+
+    @Test
+    public void merge() throws Exception {
+        SplitAndMerge.merge();
+    }
+
+}
