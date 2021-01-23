25d34b0a00ec7003b6f5eba2e0d6b9d68605d804
yunsheng <helloyunsheng@126.com>
2021-01-23 10:08:09 +0800

    mq

---
+++ src/main/java/com/example/demo/file/lock/FileLockTest.java
@@ -0,0 +1,27 @@
+package com.example.demo.file.lock;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileLock;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class FileLockTest {
+    public static void main(String[] args) throws IOException {
+        File file = new File("D:\\tmp\\lock.txt");
+        RandomAccessFile lockFile = new RandomAccessFile(file, "rw");
+
+        FileLock lock = lockFile.getChannel().tryLock(0, 1, false);
+        if (lock == null || lock.isShared() || !lock.isValid()) {
+            System.out.println("get locked failed");
+        } else {
+            System.out.println("get locked:" + lock.isValid());
+        }
+
+        Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(() -> {
+            System.out.println("running");
+        }, 10, 60, TimeUnit.SECONDS);
+    }
+}
---
+++ src/main/java/com/example/demo/rocketmq/consumeFromWhere/PushConsumer.java
@@ -0,0 +1,36 @@
+package com.example.demo.rocketmq.consumeFromWhere;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+
+/**
+ * @description: 一个新的消费组订阅一个已存在的topic，是从哪一条消息开始消费呢？
+ * @author: yunsheng
+ * @createDate: 2020/10/19
+ * @version: 1.0
+ */
+public class PushConsumer {
+    public static void main(String[] args) throws MQClientException {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("FromWhereConsumerGroup");
+        consumer.setNamesrvAddr("127.0.0.1:9876");
+        consumer.subscribe("TopicTest", "*");
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_LAST_OFFSET);
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+                System.out.printf("ReceiveMessages: %s%n", new String(msgs.get(0).getBody()));
+
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        consumer.start();
+        System.out.printf("Consumer Started.%n");
+    }
+}
